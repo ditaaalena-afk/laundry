7af3b4e771ba94376691db4912a723be99f2bc42
ditaaalena-afk <ditaaalena@gmail.com>
2025-11-28 09:08:57 +0700

    Initial commit

---
+++ admin/footer.php
@@ -0,0 +1,99 @@
+<!-- Remove the container if you want to extend the Footer to full width. -->
+<div class="container my-5">
+
+  <footer class="text-white text-center text-lg-start bg-dark">
+    <!-- Grid container -->
+    <div class="container p-4">
+      <!--Grid row-->
+      <div class="row mt-4">
+        <!--Grid column-->
+        <div class="col-lg-4 col-md-12 mb-4 mb-md-0">
+          <h5 class="text-uppercase mb-4">Tentang Laundry</h5>
+
+          <p>
+            Laundry System adalah aplikasi untuk membantu mengelola pelanggan, transaksi,
+            hingga laporan dengan lebih cepat dan efisien.
+          </p>
+
+          <p>
+            Kami hadir untuk memudahkan usaha laundry dalam melayani pelanggan dengan pelayanan terbaik.
+          </p>
+
+          <div class="mt-4">
+            <!-- Facebook -->
+            <a type="button" class="btn btn-floating btn-light btn-lg"><i class="fab fa-facebook-f"></i></a>
+            <!-- Instagram -->
+            <a type="button" class="btn btn-floating btn-light btn-lg"><i class="fab fa-instagram"></i></a>
+            <!-- Twitter -->
+            <a type="button" class="btn btn-floating btn-light btn-lg"><i class="fab fa-twitter"></i></a>
+            <!-- Whatsapp -->
+            <a type="button" class="btn btn-floating btn-light btn-lg"><i class="fab fa-whatsapp"></i></a>
+          </div>
+        </div>
+        <!--Grid column-->
+
+        <!--Grid column (gabungan Search + Kontak Kami) -->
+        <div class="col-lg-4 col-md-6 mb-4 mb-md-0">
+          <h5 class="text-uppercase mb-4 pb-1">Cari Pesanan</h5>
+
+          <!-- Search Box -->
+          <div class="form-outline form-white mb-4">
+            <input type="text" id="formControlLg" class="form-control form-control-lg" placeholder="Masukkan kode pesanan"/>
+          </div>
+
+          <!-- Kontak Kami -->
+          <h6 class="text-uppercase mb-3">Kontak Kami</h6>
+          <ul class="fa-ul" style="margin-left: 1.65em;">
+            <li class="mb-3">
+              <span class="fa-li"><i class="fas fa-home"></i></span>
+              <span class="ms-2">Jl. Mertodjidjodjo No. 05, Cening</span>
+            </li>
+            <li class="mb-3">
+              <span class="fa-li"><i class="fas fa-envelope"></i></span>
+              <span class="ms-2">support@laundrysystem.com</span>
+            </li>
+            <li class="mb-3">
+              <span class="fa-li"><i class="fas fa-phone"></i></span>
+              <span class="ms-2">+62 812 3456 7890</span>
+            </li>
+          </ul>
+        </div>
+        <!--Grid column-->
+
+        <!--Grid column-->
+        <div class="col-lg-4 col-md-6 mb-4 mb-md-0">
+          <h5 class="text-uppercase mb-4">Jam Operasional</h5>
+
+          <table class="table text-center text-white">
+            <tbody class="fw-normal">
+              <tr>
+                <td>Senin - Kamis:</td>
+                <td>08.00 - 21.00</td>
+              </tr>
+              <tr>
+                <td>Jumat - Sabtu:</td>
+                <td>08.00 - 20.00</td>
+              </tr>
+              <tr>
+                <td>Minggu:</td>
+                <td>09.00 - 21.00</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <!--Grid column-->
+      </div>
+      <!--Grid row-->
+    </div>
+    <!-- Grid container -->
+
+    <!-- Copyright -->
+    <div class="text-center p-3" style="background-color: rgba(236, 223, 223, 0.2);">
+      © 2025 Copyright:
+      <a class="text-white fw-bold" href="#">LaundrySystem.com</a>
+    </div>
+    <!-- Copyright -->
+  </footer>
+
+</div>
+<!-- End of .container -->
